58c120eca15b4e634058b5268885d3c8dd2be508
Badiru <55401789+Badiru27@users.noreply.github.com>
2022-04-08 18:03:32 +0100

    Create simple todo app

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <h1>
+            {{title}}
+        </h1>
+        <HeaderButton @toggle-button="$emit('show-add-task' )" :text="showAddTask?'Cancel': 'Add Task'" :color="showAddTask? 'red':'green'"/>
+        <!-- <HeaderButton text="Update Task" color="blue" /> -->
+    </header>
+
+</template>
+
+<script>
+import HeaderButton from './Button.vue'
+
+export default{
+    name: 'HomeHeader',
+    props:{
+        title: String,
+        showAddTask: Boolean
+    },
+    components: {
+        HeaderButton
+    }
+
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
+
+ 
